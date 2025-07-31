526ce6cb4245d8708c283392fe302f8e03809133
MrDreamhost <r.wilmer@live.nl>
2025-07-31 23:57:50 +0200

    Adjusted level 3 to fit new sizes

--- game/main.gd
+++ game/main.gd
@@ -38,6 +38,7 @@ func reset_ghosts() -> void:
 func respawn_player() -> void:
 	self.player.global_position = self.current_level.spawn_point.global_position
 	self.player.velocity = Vector2(0,0)
+	self.player.handle_respawn_when_dead()
 
 
 func reset_loop() -> void:
--- player/player.gd
+++ player/player.gd
@@ -1,8 +1,8 @@
 class_name Player extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -600.0
+const SPEED = 400.0
+const JUMP_VELOCITY = -800.0
 
 @onready var hurt_box: HurtBox = $HurtBox
 @onready var anim_player : AnimationPlayer = $PlayerAnimationPlayer
@@ -19,7 +19,10 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	move_and_slide()
-
+ 
+func handle_respawn_when_dead() -> void:
+	if %PlayerStateMachine.states[PlayerStateMachine.DEAD]:
+		%PlayerStateMachine.current_state.finished.emit(%PlayerStateMachine.states[PlayerStateMachine.IDLE])
 
 func handle_hit_received(damage: int) -> void:
 	print("Got damaged :( for %d DAMAGE" % damage)
