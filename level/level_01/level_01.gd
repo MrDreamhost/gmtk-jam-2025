e8a19faf987a7ae79abd90ae97ffbe26e9607899
Chris-GW <Chris-GW@users.noreply.github.com>
2025-08-02 00:43:30 +0200

    add level 01 music

--- level/level_01/level_01.gd
+++ level/level_01/level_01.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	super(delta)
+
+
+func _on_audio_stream_player_finished() -> void:
+	$AudioStreamPlayer.play()
