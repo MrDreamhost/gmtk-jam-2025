cb3e32a2697329ae6d3aa07731a3cd308b77a8cb
MrDreamhost <r.wilmer@live.nl>
2025-08-02 15:31:39 +0200

    Edited menus to be able to return to the main menu

--- ui/credits_menu/credits_menu.gd
+++ ui/credits_menu/credits_menu.gd
@@ -1,11 +1,5 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_back_to_menu_button_pressed() -> void:
+	LevelTransition.change_scene_to("res://ui/main_menu/main_menu.tscn")
--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -4,11 +4,6 @@ extends Control
 func _on_start_button_pressed() -> void:
 	LevelTransition.change_scene_to("res://game/main.tscn")
 
-
-func _on_how_to_play_button_pressed() -> void:
-	pass # Replace with function body.
-
-
 func _on_options_button_pressed() -> void:
 	LevelTransition.change_scene_to("res://ui/options_menu/options_menu.tscn")
 
