f58c1550d468a8602352687e13bf9e6d7695f7f6
MrDreamhost <r.wilmer@live.nl>
2025-08-02 21:20:25 +0200

    adjusted menus

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -7,6 +7,8 @@ func _on_start_button_pressed() -> void:
 func _on_options_button_pressed() -> void:
 	LevelTransition.change_scene_to("res://ui/options_menu/options_menu.tscn")
 
-
 func _on_credits_button_pressed() -> void:
 	LevelTransition.change_scene_to("res://ui/credits_menu/credits_menu.tscn")
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
