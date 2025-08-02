67030921a91c56cd8ad70c85045d424b5cd2a0ea
0xYametsu <148894158+0xYametsu@users.noreply.github.com>
2025-08-02 17:02:11 +0200

    Changed jump - Removed short hop height - Removed short hop window

--- player/player_state_machine/states/idle.gd
+++ player/player_state_machine/states/idle.gd
@@ -2,10 +2,7 @@ class_name Idle extends State
 
 
 var player: Player
-var jump_held_time: float = 0.0
-var is_jumping: bool = false
-var full_jump: float = 1.0
-var short_hop: float
+
 
 func _ready() -> void:
 	self.player = self.owner
@@ -16,18 +13,8 @@ func update_process(delta: float) -> void:
 		self.player.animated_sprite_2d.play("idle")
 
 	if Input.is_action_just_pressed("player_jump") and self.player.is_on_floor():
-		self.short_hop = self.player.player_config.short_hop_factor
-		self.is_jumping = true
-		self.jump_held_time = 0.0
-
-	if self.is_jumping:
-		self.jump_held_time += delta
-		if Input.is_action_just_released("player_jump"):
-			self.finished.emit(%PlayerStateMachine.states[PlayerStateMachine.JUMP], { "jump_factor": self.short_hop })
-			return
-		if self.jump_held_time > self.player.player_config.short_hop_window:
-			self.finished.emit(%PlayerStateMachine.states[PlayerStateMachine.JUMP], { "jump_factor": self.full_jump })
-			return
+		self.finished.emit(%PlayerStateMachine.states[PlayerStateMachine.JUMP])
+		return
 
 	var direction := Input.get_axis("player_left", "player_right")
 	if direction:
@@ -38,4 +25,3 @@ func update_process(delta: float) -> void:
 
 func enter(_data := {}) -> void:
 	super()
-	self.is_jumping = false
--- player/player_state_machine/states/run.gd
+++ player/player_state_machine/states/run.gd
@@ -2,10 +2,7 @@ class_name Run extends State
 
 
 var player: Player
-var jump_held_time: float = 0.0
-var is_jumping: bool = false
-var full_jump: float = 1.0
-var short_hop: float
+
 
 func _ready() -> void:
 	self.player = self.owner
@@ -13,18 +10,8 @@ func _ready() -> void:
 
 func update_physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("player_jump") and self.player.is_on_floor():
-		self.short_hop = self.player.player_config.short_hop_factor
-		self.is_jumping = true
-		self.jump_held_time = 0.0
-
-	if self.is_jumping:
-		self.jump_held_time += delta
-		if Input.is_action_just_released("player_jump"):
-			self.finished.emit(%PlayerStateMachine.states[PlayerStateMachine.JUMP], { "jump_factor": self.short_hop })
-			return
-		if self.jump_held_time > self.player.player_config.short_hop_window:
-			self.finished.emit(%PlayerStateMachine.states[PlayerStateMachine.JUMP], { "jump_factor": self.full_jump })
-			return
+		self.finished.emit(%PlayerStateMachine.states[PlayerStateMachine.JUMP])
+		return
 
 	var direction := Input.get_axis("player_left", "player_right")
 	if direction:
@@ -64,7 +51,6 @@ func update_physics_process(delta: float) -> void:
 
 func enter(_data := {}) -> void:
 	super()
-	self.is_jumping = false
 
 
 func is_turning(direction: float) -> bool:
