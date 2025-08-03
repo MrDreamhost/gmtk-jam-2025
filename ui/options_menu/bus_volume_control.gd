d97f0fd331cb98fd820aff305604d1ea254d8063
MrDreamhost <r.wilmer@live.nl>
2025-08-03 10:56:09 +0200

    Adjusted all the menus

--- ui/options_menu/bus_volume_control.gd
+++ ui/options_menu/bus_volume_control.gd
@@ -5,6 +5,7 @@ extends Container
 @export var bus: Bus = Bus.MASTER
 @export var test_audio: AudioStream
 
+@onready var hover_sound : AudioStreamPlayer = $AudioStreamPlayer2
 @onready var volume_slider = $VolumeSlider as HSlider
 
 enum Bus {
@@ -15,7 +16,6 @@ enum Bus {
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Label.text = AudioServer.get_bus_name(bus) + " Volume:"
-	$MutedCheckButton.button_pressed = AudioServer.is_bus_mute(bus)
 	$AudioStreamPlayer.stream = test_audio
 	$AudioStreamPlayer.bus = AudioServer.get_bus_name(bus)
 	
@@ -28,7 +28,6 @@ func _on_muted_check_button_toggled(toggled_on: bool):
 
 func _on_volume_slider_value_changed(value: float):
 	var muted = value <= volume_slider.min_value
-	$MutedCheckButton.button_pressed = muted
 	AudioServer.set_bus_volume_db(bus, linear_to_db(value))
 	AudioServer.set_bus_mute(bus, muted)
 	if(not $AudioStreamPlayer.playing):
@@ -38,3 +37,7 @@ func _on_volume_slider_value_changed(value: float):
 func _on_volume_slider_drag_ended(value_changed: bool) -> void:
 	if bus == Bus.VOICE:
 		GlobalAudioManager.play_vo("any")
+
+
+func _on_volume_slider_mouse_entered() -> void:
+	hover_sound.play()
