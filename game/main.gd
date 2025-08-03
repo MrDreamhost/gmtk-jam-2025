f54f222290af190bc45115d09380ea534dbaa19c
0xYametsu <148894158+0xYametsu@users.noreply.github.com>
2025-08-03 18:58:27 +0200

    Reverted apple fixes

--- game/main.gd
+++ game/main.gd
@@ -153,7 +153,7 @@ func turn_clock_back() -> void:
 
 func _on_goal_reached(_next_level_file: String) -> void:
 	next_level_file = _next_level_file
-	play_victory_eat_apple_animation()
+	#play_victory_eat_apple_animation()
 	loop_timer.stop()
 	self.animated_timer.stop()
 	self.animated_timer.visible = false
